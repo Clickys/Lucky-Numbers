daef6bc0d1608206ed4323b35ab60dc0f41a482d
clickys <koc.clicky@gmail.com>
2017-09-30 02:36:08 +0300

    Random numbers + player number work - Message winner work

--- script.js
+++ script.js
@@ -1,20 +1,82 @@
 //Create an object that will organize informations about the lucky numbers
 let luckyNumbers = {
     numbers: [],
+    playerLuckyNumber: 0,
+    winner: false,
+    congrantsMessage: 'YOU ARE THE WINNER !!!!',
+    tryAgainMessage: 'Try Again',
 
     randomNumbers: function () {
         for (let i = 0; i < 5; i++) {
-            this.numbers.push(Math.floor((Math.random() * 100) + 1));
+            var randomNumber = Math.floor((Math.random() * 100) + 1);
+            this.numbers.push(randomNumber);
         }
     },
 
-    showLuckyNumbers: function() {
-        for (let i = 0; i < this.numbers.length; i++) {
-            console.log(this.numbers[i]);
-        }
-    }
+    playerNumber: function () {
+        var randomNumber = Math.floor((Math.random() * 100) + 1);
+        return this.playerLuckyNumber = randomNumber;
+    },
 
+    winnerNumber: function () {
+        this.numbers.forEach((number) => {
+            if (number === this.playerLuckyNumber) {
+                alert('Lucky number');
+                this.winner = true;
+            }
+        })
+        luckyNumbers.numbers = [];
+    }
 
 };
 
+let handler = {
 
+    spinButton: function () {
+        debugger;
+        view.clearLuckyNumberMessage();
+        view.displayPlayerNumber();
+        view.displayLuckyNumbers();
+        view.displayLuckyNumberMessage();
+
+
+    }
+}
+
+let view = {
+
+    displayLuckyNumbers: function () {
+        luckyNumbers.randomNumbers();
+        let liElements = Array.from(document.querySelectorAll('li'));
+        let i = 0;
+
+        liElements.forEach(function (li) {
+            li.textContent = luckyNumbers.numbers[i];
+            i++;
+        })
+
+    },
+
+    displayPlayerNumber: function () {
+        luckyNumbers.playerNumber();
+        let playerNumberPosition = document.getElementById('playerNumber');
+        playerNumber.textContent = '';
+        playerNumber.textContent = luckyNumbers.playerLuckyNumber;
+    },
+
+    displayLuckyNumberMessage: function () {
+        luckyNumbers.winnerNumber();
+        let congrantsMessagePosition = document.getElementById('congrantsMessagePosition');
+        if (luckyNumbers.winner) {
+            congrantsMessagePosition.textContent = luckyNumbers.congrantsMessage;
+
+        }
+
+    },
+
+    clearLuckyNumberMessage: function () {
+        let congrantsMessagePosition = document.getElementById('congrantsMessagePosition');
+       console.log(congrantsMessagePosition);
+        congrantsMessagePosition.innerHTML = luckyNumbers.tryAgainMessage;
+    }
+}
